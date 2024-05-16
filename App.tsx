31a9199fdce4e698146bd9859a13d03d5a17ed4b
Stephen Montana <stephen.montana@sunnova.com>
2024-05-16 13:50:36 -0500

    init

---
+++ App.tsx
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const scrapeIt = require("scrape-it");
+const nodemailer = require("nodemailer");
+
+const user = process.env.EMAIL;
+const pass = process.env.PW;
+const site = process.env.SITE;
+const titleSelector = process.env.TITLE_SELECTOR;
+const descSelector = process.env.DESC_SELECTOR;
+const textToCheck = process.env.TEXT_TO_CHECK;
+const fromEmail = process.env.FROM_EMAIL;
+const toEmails = process.env.TO_EMAILS;
+const subject = process.env.SUBJECT;
+const body = process.env.BODY;
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user,
+    pass,
+  },
+});
+
+(async () => {
+  const { data } = await scrapeIt(site, {
+    title: {
+      selector: titleSelector,
+    },
+    desc: {
+      selector: descSelector,
+    },
+  });
+
+  if (data.desc === textToCheck) {
+    console.log("not available yet");
+  } else {
+    const mailOptions = {
+      from: fromEmail,
+      to: toEmails,
+      subject: subject,
+      text: body,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email: ", error);
+      } else {
+        console.log("Email sent: ", info.response);
+      }
+    });
+  }
+})();
